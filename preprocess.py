75ff74b4edb00e6f9438be4598c007df20038ec6
iniadewumi <72786828+iniadewumi@users.noreply.github.com>
2022-04-20 17:37:01 -0500

    updated data

--- preprocess.py
+++ preprocess.py
@@ -9,23 +9,46 @@ from ip_re import ip_re, short_re
 from urllib.parse import urlparse
 from io import StringIO
 import requests
+import category_encoders as ce
+
+
+pd.options.display.max_columns = 100
 
 ROOT = pathlib.Path().resolve()
 DATA = ROOT / 'Data'
 CSV = DATA / 'CSVs'
 MALIC = CSV / 'Malicious Datasets'
 
-
+# https://www.icdsoft.com/blog/what-are-the-most-popular-tlds-domain-extensions/
+#TOP LEVEL DOMAINS BADNESS INDEX
 
 class Preprocessor:
     def __init__(self):
         self.df = pd.read_csv(MALIC/'malicious_phish.csv')
         self.df = self.df[self.df.type != 'label']
+        self.type_map = {'type_1': 'phishing', 'type_2': 'benign', 'type_3':'defacement', 'type_4':'malware'}
         print(f"\nMissing Data Check:\n{self.df.isnull().sum()}")
         print(f'\n\nDataset desription \n{self.df.describe()}')
         types = self.df['type'].value_counts()
+        
         plt.bar(types.index, height=types, color=['navy',  'blue', 'purple', 'red'])
 
+
+    def target_encode(self, var, target):
+        print(f"Target Encoding {var}...")
+        encoder = ce.OneHotEncoder()
+        enc_target = encoder.fit_transform(self.df[target])
+        enc_target.rename(columns=self.type_map, inplace=True)
+        
+        for targ in enc_target.columns:
+            enc = ce.TargetEncoder()
+            enc.fit(self.df[var], enc_target[targ])
+            
+            temp = enc.transform(self.df[var])
+            temp.columns = [f'enc_{var}' for x in temp.columns]
+            X = pd.concat([self.df[var], temp], axis=1)
+        self.df = pd.concat([self.df, X], axis=1)
+        self.df.drop(var, axis=1, inplace=True)
 
 
     # get_tld(x, as_object = True, fail_silently=False,fix_protocol=True)
@@ -34,20 +57,25 @@ class Preprocessor:
             res = get_tld(url, as_object = True, fail_silently=False,fix_protocol=True)
             domain = res.parsed_url.netloc
             scheme = res.parsed_url.scheme
-            path =   res.parsed_url.path
+            path =   len(res.parsed_url.path)
             normal = 0 if re.search(str(urlparse(url).hostname), url) else 1
+            tld = res.tld
 
         except Exception:
             domain = None
-            scheme = url.split(":")[0] if len(url.split(":")[0])>1 else ""
-            path = None
+            scheme = url.split(":")[0] if len(url.split(":")[0])<5 else ""
+            path = 0
             normal = 1
+            tld = ''
         digits = len(re.findall('[0-9]+', url))
         letters = len(re.findall('[A-Za-z]', url))
         contains_ip = ip_re(url)
         is_shortened = short_re(url)
 
-        return [scheme, domain, path, normal, digits, letters, contains_ip, is_shortened]
+        return [scheme, domain, path, normal, digits, letters, contains_ip, is_shortened, tld]
+    def get_scheme(self):
+        self.df['scheme'] = self.df['url'].apply(lambda x: urlparse(x).scheme)
+        
     def workflow(self):
         print("\n\nRemoving www...")
         self.df['url'] = self.df['url'].str.replace('www.', '', regex=True)
@@ -56,18 +84,21 @@ class Preprocessor:
         self.df['url_length'] = self.df['url'].apply(lambda x: len(str(x)))
 
         print("Extracting URL components...")
-        self.df[['scheme', 'domain', 'path', "normal", "digits", "letters", "contains_ip", "is_shortened"]] = self.df['url'].apply(lambda url: self.extract_domain(url)).tolist()
+        self.df[['scheme', 'domain', 'path', "normal", "digits", "letters", "contains_ip", "is_shortened", 'tld_normal']] = self.df['url'].apply(lambda url: self.extract_domain(url)).tolist()
 
-        print("Finding secure links...")
+        print("\nFinding secure links...")
         self.df['secure'] = self.df['scheme'].apply(lambda x: 1 if x=='https' else 0)
 
-        print("Finding and counting special characters")
+        print("Finding and counting special characters\n")
         for c in list(string.punctuation)+["//"]:
             self.df[c] = self.df['url'].apply(lambda i: i.count(c))
 
         desc = self.df.describe().T['mean']
-        new_cols = ['url', 'type', 'scheme', 'domain', 'path', 'normal', 'digits', 'letters', "contains_ip", "is_shortened"] + list(desc[desc >0.01].index )
+        new_cols = ['url', 'type', 'scheme', 'domain', 'path', 'normal', 'digits', 'letters', "contains_ip", "is_shortened", 'tld_normal'] + list(desc[desc >0.01].index )
         self.df = self.df[new_cols]
+        
+        self.target_encode('tld_normal', 'type')
+        self.target_encode('scheme', 'type')
         self.save_df()
         
     def save_df(self):
@@ -79,3 +110,22 @@ if __name__ == '__main__':
     prep = Preprocessor()
     prep.workflow()
     print("\n\nSuccessfully Completed preprocessing!\nReady for training\n\n")
+    
+"""
+What is the definition of Monte Carlo Simulation?
+What are the parameters and plausible range of different random number generators (distributions)?
+What is the difference between normal, truncated normal, and triangular distribution?
+How do we simulate random numbers from empirical discrete distributions?
+How does sample()function in R work? What are the inputs and outputs?
+Difference between sampling with and without replacement in a simulation?
+What are the properties of pseudo-random numbers?
+What does the term such as IID and stationary mean?
+How are the coding elements of the newsvendor model designed? What is the decision variable? How to find optimal? What are the size and dimensions of each object?
+How do we determine that a distribution is a good fit for observed data? How do we compare different options? What test do we apply? What plots do we use?
+How does bootstrapping work? What is the sample size in each iteration? How does replicate function work? How to measure confidence interval?
+How does LCG work? Can you do it by hand?
+How do we measure risk by Monte Carlo Simulation?
+How does inverse transformation work for generating random variates? What are its parameters and other inputs? How it can be implemented in R?
+How to simulate based on the output of input analysis?
+
+"""
--- train.py
+++ train.py
@@ -2,32 +2,111 @@ import pandas as pd
 import pathlib
 import matplotlib.pyplot as plt
 from scipy import sparse
-from sklearn.preprocessing import LabelBinarizer
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.model_selection import cross_validate
 import seaborn as sns
+
+from tld import get_tld, is_tld
+pd.options.display.max_columns = 100
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+import pickle
+# manual nested cross-validation for random forest on a classification dataset
+from numpy import mean, std
+
 
 ROOT = pathlib.Path().resolve()
 DATA = ROOT / 'Data'
 CSV = DATA / 'CSVs'
 MALIC = CSV / 'Malicious Datasets'
+MODELS = DATA / "Models"
 
 df = pd.read_csv(MALIC/'processed_dataframe.csv')
+
+
+
+
+class Training:
+    def __init__(self):
+        self.df = pd.read_csv(MALIC/'processed_dataframe.csv')
+    
+# train = Training()
+
+
+X = df.drop(['url', 'type',  'domain'], axis=1)
+y = df['type']
+
+types = y
+plt.bar(types.index, height=types, color=['navy',  'blue', 'purple', 'red'])
+
+
+kf = KFold(n_splits=5)
+
+outer_results = list()
+
+for train_index, test_index in kf.split(X):
+    # print("TRAIN:", train_index, "TEST:", test_index)
+    X_train, X_test = X.loc[train_index], X.loc[test_index]
+    y_train, y_test = y.loc[train_index], y.loc[test_index]
+
+   
+    cv_inner = KFold(n_splits=3, shuffle=True, random_state=1)
+    # define the model
+    model = RandomForestClassifier(random_state=1, verbose=True)
+    
+    # define search space
+    space = dict()
+    space['n_estimators'] = [10, 100, 500]
+    space['max_features'] = [2, 4, 6]
+    
+    # define search
+    search = GridSearchCV(model, space, scoring='accuracy', cv=cv_inner, refit=True, verbose=True)
+    result = search.fit(X_train, y_train)
+    
+    # get the best performing model fit on the whole training set
+    best_model = result.best_estimator_
+    yhat = best_model.predict(X_test)
+    # evaluate the model
+    acc = accuracy_score(y_test, yhat)
+    # store the result
+    outer_results.append(acc)
+    # report progress
+    print('acc=%.3f, est=%.3f, cfg=%s' % (acc, result.best_score_, result.best_params_))
+# summarize the estimated performance of the model
+print('Accuracy: %.3f (%.3f)' % (mean(outer_results), std(outer_results)))
+
+
+with open(MODELS/"random_forest.pickle", "wb") as f:
+    pickle.dump(best_model, f)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=111)
+
+# estimator = RandomForestClassifier(n_estimators=200)
+# estimator.fit(X,y)
+# cross_validate(estimator, X, y)
+# cross_validate(estimator, X_test, y_test)
+
+
+
     
 
-# df = pd.read_csv(CSV/'processed_dataframe.csv')
-
-y_dense = LabelBinarizer().fit_transform(df['type'])
-y_sparse = sparse.csr_matrix(y_dense)
-df['Cat'] = y_sparse
+# y_dense = LabelBinarizer().fit_transform(df['type'])
+# # y_sparse = sparse.csr_matrix(y_dense)
+# # df['Cat'] = y_sparse
 
 
-#TRAIN
-X = df.drop(['url','type','Category','domain'],axis=1)#,'type_code'
-y = df['Category']
+# #TRAIN
+# X = df.drop(['url','type','Category','domain'],axis=1)
+# y = df['Category']
 
-plt.figure(figsize=(12,6))
-sns.boxplot(df=df,x='Type',y='URL_LENGTH')
-plt.figure(figsize=(12,6))
-sns.boxplot(df=df,x='Type',y='NUMBER_SPECIAL_CHARACTERS')
-plt.figure(figsize=(15, 15))
-sns.heatmap(df.corr(), linewidths=.5)
+# plt.figure(figsize=(12,6))
+# sns.boxplot(df=df,x='Type',y='URL_LENGTH')
+# plt.figure(figsize=(12,6))
+# sns.boxplot(df=df,x='Type',y='NUMBER_SPECIAL_CHARACTERS')
+# plt.figure(figsize=(15, 15))
+# sns.heatmap(df.corr(), linewidths=.5)
